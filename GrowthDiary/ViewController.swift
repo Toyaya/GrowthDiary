384b75279667d0c5a123f2b4a4e18e43fe55a3f1
Toyaya <632731678@qq.com>
2019-09-16 09:59:14 +0800

    Initial Commit

---
+++ GrowthDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrowthDiary
+//
+//  Created by Toya Sakurai on 2019/9/16.
+//  Copyright © 2019 Toya Sakurai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
